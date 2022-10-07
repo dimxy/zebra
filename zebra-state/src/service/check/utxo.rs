64e9d6b5ab1ac33886042e8aea9e4d85fd0f4bfa
DeckerSU <deckersu@protonmail.com>
2022-10-07 02:08:44 +0200

    allow transparent coinbase spend
    TODO: check that MIN_TRANSPARENT_COINBASE_MATURITY still enabled (!)
    
    spend_height for SomeTransparentOutputs?
    
    Example:
    
    ```
    WARN {zebrad="7ff08cd" net="Main"}:sync:try_to_sync: zebrad::components::sync: error downloading and verifying block e=Invalid { error: Block(Commit(CloneError { source: CommitBlockError(UnshieldedTransparentCoinbaseSpend { outpoint: OutPoint { hash: transaction::Hash("a3fa9f5b8a6f015754eb215aa7e3f3e5b85a5b0714a031caf303ebf91f9b1b55"), index: 0 } }) })), height: Height(3025616), hash: block::Hash("00000000aff02c8493761eacbb62f987e31e0c15b30c89405a013b6fb010d4b0") }
    ```

--- zebra-state/src/service/check/utxo.rs
+++ zebra-state/src/service/check/utxo.rs
@@ -193,29 +193,31 @@ pub fn transparent_coinbase_spend(
     spend_restriction: transparent::CoinbaseSpendRestriction,
     utxo: transparent::OrderedUtxo,
 ) -> Result<transparent::OrderedUtxo, ValidateContextError> {
-    if !utxo.utxo.from_coinbase {
-        return Ok(utxo);
-    }
+    Ok(utxo)
+    // if !utxo.utxo.from_coinbase {
+    //     return Ok(utxo);
+    // }
 
-    match spend_restriction {
-        OnlyShieldedOutputs { spend_height } => {
-            let min_spend_height =
-                utxo.utxo.height + block::Height(MIN_TRANSPARENT_COINBASE_MATURITY);
-            let min_spend_height =
-                min_spend_height.expect("valid UTXOs have coinbase heights far below Height::MAX");
-            if spend_height >= min_spend_height {
-                Ok(utxo)
-            } else {
-                Err(ImmatureTransparentCoinbaseSpend {
-                    outpoint,
-                    spend_height,
-                    min_spend_height,
-                    created_height: utxo.utxo.height,
-                })
-            }
-        }
-        SomeTransparentOutputs => Err(UnshieldedTransparentCoinbaseSpend { outpoint }),
-    }
+    // match spend_restriction {
+    //     OnlyShieldedOutputs { spend_height } => {
+    //         let min_spend_height =
+    //             utxo.utxo.height + block::Height(MIN_TRANSPARENT_COINBASE_MATURITY);
+    //         let min_spend_height =
+    //             min_spend_height.expect("valid UTXOs have coinbase heights far below Height::MAX");
+    //         if spend_height >= min_spend_height {
+    //             Ok(utxo)
+    //         } else {
+    //             Err(ImmatureTransparentCoinbaseSpend {
+    //                 outpoint,
+    //                 spend_height,
+    //                 min_spend_height,
+    //                 created_height: utxo.utxo.height,
+    //             })
+    //         }
+    //     }
+    //     SomeTransparentOutputs => Err(UnshieldedTransparentCoinbaseSpend { outpoint }),
+    // }
+
 }
 
 /// Reject negative remaining transaction value.
