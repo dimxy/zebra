fd65d2e56490a66dad134e5f5bc515294ac9a3e6
dimxy <dimxy@komodoplatform.com>
2023-04-23 12:12:09 +0500

    log start hash

--- zebra-state/src/service/read/find.rs
+++ zebra-state/src/service/read/find.rs
@@ -547,7 +547,7 @@ fn komodo_best_relevant_chain(
         .take(depth)
         .collect();
     if best_relevant_chain.len() < depth {
-        info!("best_relevant_chain.len() {} depth {}", best_relevant_chain.len(), depth);
+        info!("best_relevant_chain.len() {} depth {} start_hash {:?}", best_relevant_chain.len(), depth, start_hash);
         return Err(BoxError::from("Zebra's state does not contain requested number of blocks"));
     }
 
